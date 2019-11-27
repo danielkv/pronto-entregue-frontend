a67e0acf82d6d8ba10893cb0e7226cdd836d5e5b
Daniel K. Guolo <daniel_kv@hotmail.com>
2019-11-27 18:57:56 -0300

    fix build

---
+++ build/precache-manifest.371d27ce94b67fe055fafe0fd63da30c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5416825f70e518ca561b150c0de021",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beca35009bb3114c126e",
+    "url": "/static/css/main.eabb592e.chunk.css"
+  },
+  {
+    "revision": "6ae4117567e4c5fc7264",
+    "url": "/static/js/2.30859fcf.chunk.js"
+  },
+  {
+    "revision": "beca35009bb3114c126e",
+    "url": "/static/js/main.28dcf7be.chunk.js"
+  },
+  {
+    "revision": "10a4e6f39cba6fb935bd",
+    "url": "/static/js/runtime~main.ee380e59.js"
+  },
+  {
+    "revision": "6872e144c9426fa83f6cc7b4dc40af6b",
+    "url": "/static/media/bg1.6872e144.jpg"
+  },
+  {
+    "revision": "0b01608b811a7141168f4e2b7b796648",
+    "url": "/static/media/bg2.0b01608b.jpg"
+  },
+  {
+    "revision": "167d688ad01afd00d54a20c08c356b40",
+    "url": "/static/media/bg3.167d688a.jpg"
+  },
+  {
+    "revision": "47a1d8e5ea033309d97ee64fa3b7e665",
+    "url": "/static/media/logo-full.47a1d8e5.png"
+  }
+]);
