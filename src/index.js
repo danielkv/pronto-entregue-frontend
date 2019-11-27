a98dd18e573f93764f742ef9551692bf8ace16e8
Daniel K. Guolo <daniel_kv@hotmail.com>
2019-11-27 18:34:44 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-//import 'react-hot-loader/patch';
+/* eslint-disable import/first */
+import dotenv from 'dotenv';
+dotenv.config();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import ReactDOM from '@hot-loader/react-dom';
 import { ApolloProvider } from "@apollo/react-hooks";
+
 
 import apolloCliente from './services/server';
 import Router from './router';
