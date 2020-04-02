ba9de04f1a8edc23cbaabf25e6bd5833bc95f93e
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-04-01 21:51:29 -0300

    ssl certificate

--- server.js
+++ server.js
@@ -5,17 +5,23 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use((req, res, next) => {
+	if (req.secure)
+		next();
+	else
+		res.redirect('https://' + req.headers.host + req.url);
+})
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
+	if (process.env.SETUP) return res.sendStatus(400)
 	return res.send('pong');
 });
 app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, ()=> {
-	console.log(`Port: ${port}`);
-});
+app.listen(port);
