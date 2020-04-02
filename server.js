3b59479b5b7d09faeb8d031c905709b970defd21
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-04-01 22:55:54 -0300

    ensureSecure

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 8080;
 
 function forceSecure(req, res, next) {
 	//Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
-	if (req.get('X-Forwarded-Proto')=='https' || req.hostname == 'localhost') {
+	if (req.header('x-forwarded-proto')!=='https' || req.hostname === 'localhost') {
 		//Serve Angular App by passing control to the next middleware
 		next();
-	} else if(req.get('X-Forwarded-Proto')!='https' && req.get('X-Forwarded-Port')!='443'){
+	} else {
 		//Redirect if not HTTP with original request URL
-		res.redirect('https://' + req.hostname + req.url);
+		res.redirect('https://' + req.hostname + req.url, 301);
 	}
 }
 
