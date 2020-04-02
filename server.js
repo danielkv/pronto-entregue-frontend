ffbd9c5897da281ebf13ad535d4596be85582837
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-04-01 22:18:21 -0300

    forceSecure

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const favicon = require('express-favicon');
-const forceSecure = require('express-force-https');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+function forceSecure(req, res, next) {
+	//Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
+	if (req.get('X-Forwarded-Proto')=='https' || req.hostname == 'localhost') {
+		//Serve Angular App by passing control to the next middleware
+		next();
+	} else if(req.get('X-Forwarded-Proto')!='https' && req.get('X-Forwarded-Port')!='443'){
+		//Redirect if not HTTP with original request URL
+		res.redirect('https://' + req.hostname + req.url);
+	}
+}
 
 app.use(forceSecure);
 
