0593a95e059e4fc94676afb3fbce4f0c52fe628f
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-04-23 00:20:42 -0300

    subscrition transport

--- src/services/apolloClient/webSocketLink.js
+++ src/services/apolloClient/webSocketLink.js
@@ -1,10 +1,12 @@
 import { WebSocketLink } from 'apollo-link-ws';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 
-const host = process.env.NODE_ENV === 'production' ? 'wss://pronto-entregue-backend.herokuapp.com/graphql' : `ws://localhost:4000/graphql`;
+const GRAPHQL_ENDPOINT = process.env.NODE_ENV === 'production' ? 'wss://pronto-entregue-backend.herokuapp.com/graphql' : `ws://localhost:4000/graphql`;
 
-export default new WebSocketLink({
-	uri: host,
-	options: {
-		reconnect: true
-	}
+const client = new SubscriptionClient(GRAPHQL_ENDPOINT, {
+	reconnect: true,
+	reconnectionAttempts: 5,
+	timeout: 40000
 });
+
+export default new WebSocketLink(client);
