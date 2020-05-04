6852a3b821431308a962d8ec7c0deede6d920bf9
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-05-04 20:01:12 -0300

    fix https

--- src/services/apolloClient/uploadLink.js
+++ src/services/apolloClient/uploadLink.js
@@ -1,5 +1,5 @@
 import { createUploadLink } from 'apollo-upload-client';
 
-const host = process.env.NODE_ENV === 'production' ? 'http://ec2-52-67-199-173.sa-east-1.compute.amazonaws.com:4000/graphql' : 'http://localhost:4000/graphql';
+const host = process.env.NODE_ENV === 'production' ? 'https://api.prontoentregue.com.br/graphql' : 'http://localhost:4000/graphql';
 
 export default createUploadLink({ uri: host });
--- src/services/apolloClient/webSocketLink.js
+++ src/services/apolloClient/webSocketLink.js
@@ -1,7 +1,7 @@
 import { WebSocketLink } from 'apollo-link-ws';
 import { SubscriptionClient } from 'subscriptions-transport-ws';
 
-const GRAPHQL_ENDPOINT = process.env.NODE_ENV === 'production' ? 'ws://ec2-52-67-199-173.sa-east-1.compute.amazonaws.com:4000/graphql' : `ws://localhost:4000/graphql`;
+const GRAPHQL_ENDPOINT = process.env.NODE_ENV === 'production' ? 'wss://api.prontoentregue.com.br/graphql' : `ws://localhost:4000/graphql`;
 
 const client = new SubscriptionClient(GRAPHQL_ENDPOINT, {
 	reconnect: true,
