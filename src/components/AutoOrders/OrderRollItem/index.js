d8b14623b029afca46785059e7718914f30e7820
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-05-22 13:53:53 -0300

    fix

--- src/components/AutoOrders/OrderRollItem/index.js
+++ src/components/AutoOrders/OrderRollItem/index.js
@@ -70,7 +70,7 @@ export default function OrderRollItem({ item: order }) {
 						</Fragment>
 					))}
 				</div>
-				<div>
+				<div style={{ width: '40%' }}>
 					<Typography style={{ fontWeight: 'bold' }}>{order.user.fullName}</Typography>
 					{Boolean(order.user.phones && order.user.phones.length) && <div><Typography variant='caption'>{order.user.phones[0].value}</Typography></div>}
 					<Typography variant='caption'>{order.user.email}</Typography>
@@ -84,17 +84,25 @@ export default function OrderRollItem({ item: order }) {
 									<Typography variant='subtitle2'>{order.address.district}</Typography>
 									<Typography variant='subtitle2'>{`${order.address.city} - ${order.address.state}`}</Typography>
 									<Typography variant='subtitle2'>{order.address.zipcode}</Typography>
-									<div style={{ marginTop: 10 }}>
-										<div style={{ fontWeight: 'bold', display: 'flex', flexDirection: 'row' }}>
-											<Typography style={{ fontWeight: 'bold', marginRight: 5 }}>Pagamento:</Typography>
-											{order.paymentMethod && <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
-												<img alt='Forma de pagamento' height={20} style={{ marginRight: 5 }} src={order.paymentMethod.image} />
-												<Typography variant='caption'>{`${order.paymentMethod.displayName}`}</Typography>
-											</div>}
-										</div>
-										<div style={{ marginLeft: 20 }}>
-											<table style={{ width: '100%' }}>
-												{!!order.discount &&
+									
+									
+								</div>
+							)}
+
+						<div>
+							<div style={{ marginTop: 10 }}>
+								<div style={{ fontWeight: 'bold', display: 'flex', flexDirection: 'row' }}>
+									<Typography style={{ fontWeight: 'bold', marginRight: 5 }}>Pagamento:</Typography>
+									{order.paymentMethod && <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+										<img alt='Forma de pagamento' height={20} style={{ marginRight: 5 }} src={order.paymentMethod.image} />
+										<Typography variant='caption'>{`${order.paymentMethod.displayName}`}</Typography>
+									</div>}
+								</div>
+										
+							</div>
+							<div style={{ marginLeft: 20 }}>
+								<table style={{ width: '100%' }}>
+									{!!order.discount &&
 													<>
 														<tr>
 															<td><Typography variant='caption' style={{ fontWeight: 'bold' }}>Subtotal</Typography></td>
@@ -105,22 +113,21 @@ export default function OrderRollItem({ item: order }) {
 															<td style={{ textAlign: 'right' }}><Typography variant='caption'>{numeral(order.discount).format('$0,0.00')}</Typography></td>
 														</tr>
 													</>
-												}
-												{!!order.price && <tr>
-													<td><Typography variant='caption' style={{ fontWeight: 'bold' }}>Total</Typography></td>
-													<td style={{ textAlign: 'right' }}><Typography style={{ fontWeight: 'bold' }}>{numeral(order.price).format('$0,0.00')}</Typography></td>
-												</tr>}
-											</table>
-										</div>
-									</div>
-									{Boolean(order.message) && (
-										<div style={{ marginTop: 10 }}>
-											<Typography style={{ fontWeight: 'bold' }}>Observações: </Typography>
-											<Typography variant='caption'>{order.message}</Typography>
-										</div>
-									)}
-								</div>
-							)}
+									}
+									<tr>
+										<td><Typography variant='caption' style={{ fontWeight: 'bold' }}>Total (cobrado do cliente)</Typography></td>
+										<td style={{ textAlign: 'right' }}><Typography style={{ fontWeight: 'bold' }}>{numeral(order.price).format('$0,0.00')}</Typography></td>
+									</tr>
+								</table>
+							</div>
+						</div>
+
+						{Boolean(order.message) && (
+							<div style={{ marginTop: 10 }}>
+								<Typography style={{ fontWeight: 'bold' }}>Observações: </Typography>
+								<Typography variant='caption'>{order.message}</Typography>
+							</div>
+						)}
 					</div>
 				</div>
 			</div>
