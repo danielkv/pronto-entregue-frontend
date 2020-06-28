030467b27e69f7fb891cf9b8cfd2954e6cf1719e
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-06-28 15:19:06 -0300

    fix server

---
+++ src/config/server.js
@@ -0,0 +1,12 @@
+const serverConfig = {};
+
+if (process.env.NODE_ENV === 'production') {
+	serverConfig.host = 'https://api.prontoentregue.com.br/graphql';
+	serverConfig.webSocket = 'wss://api.prontoentregue.com.br/graphql';
+} else if (process.env.NODE_ENV === 'staging') {
+	serverConfig.host = 'http://ec2-18-228-38-55.sa-east-1.compute.amazonaws.com:4000/graphql';
+	serverConfig.webSocket = 'wss://ec2-18-228-38-55.sa-east-1.compute.amazonaws.com:4000/graphql';
+} else {
+	serverConfig.host = 'http://localhost:4000/graphql';
+	serverConfig.webSocket = 'ws://localhost:4000/graphql';
+}
--- src/services/apolloClient/uploadLink.js
+++ src/services/apolloClient/uploadLink.js
@@ -1,6 +1,5 @@
 import { createUploadLink } from 'apollo-upload-client';
 
-const host = process.env.NODE_ENV === 'production' ? 'https://api.prontoentregue.com.br/graphql' : 'http://localhost:4000/graphql';
-//const host = process.env.NODE_ENV === 'production' ? 'https://api.prontoentregue.com.br/graphql' : 'http://ec2-18-229-163-189.sa-east-1.compute.amazonaws.com:4000/graphql';
+import serverConfig from '../../config/server';
 
-export default createUploadLink({ uri: host });
+export default createUploadLink({ uri: serverConfig.host });
