fb065232b437e9d8fadc50b4fe402465b0cb87af
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-07-02 01:21:03 -0300

    fix server

--- src/config/server.js
+++ src/config/server.js
@@ -1,14 +1,11 @@
 const serverConfig = {};
 
-/* serverConfig.host = 'https://api.prontoentregue.com.br/graphql';
-serverConfig.webSocket = 'wss://api.prontoentregue.com.br/graphql'; */
-
 if (process.env.NODE_ENV === 'production') {
 	serverConfig.host = 'https://api.prontoentregue.com.br/graphql';
 	serverConfig.webSocket = 'wss://api.prontoentregue.com.br/graphql';
 
-	serverConfig.host = 'https://staging.prontoentregue.com.br/graphql';
-	serverConfig.webSocket = 'wss://staging.prontoentregue.com.br/graphql';
+	/* serverConfig.host = 'https://staging.prontoentregue.com.br/graphql';
+	serverConfig.webSocket = 'wss://staging.prontoentregue.com.br/graphql'; */
 } else {
 	serverConfig.host = 'http://localhost:4000/graphql';
 	serverConfig.webSocket = 'ws://localhost:4000/graphql';
