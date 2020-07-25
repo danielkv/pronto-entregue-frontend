be1746b19e3008ef110feeb4e7e726e59bf1cf80
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-07-25 13:50:45 -0300

    optional chaining

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,18 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader')
 
+const { useBabelRc, override } = require('customize-cra');
+
 /* config-overrides.js */
-module.exports = function override (config, env) {
-	config = rewireReactHotLoader(config, env);
-	
-	config.resolve.alias = {
-		...config.resolve.alias,
-		'react-dom': '@hot-loader/react-dom'
+module.exports = override(
+	useBabelRc(),
+	(config, env) => {
+		config = rewireReactHotLoader(config, env);
+		
+		config.resolve.alias = {
+			...config.resolve.alias,
+			'react-dom': '@hot-loader/react-dom'
+		}
+		
+		return config
 	}
-	return config
-}
+)
